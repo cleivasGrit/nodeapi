241b9c8b1d35d0c94709e583bd9bbd425fde5d72
cleivasGrit <clara.leivas@gritacademy.se>
2024-09-30 17:28:01 +0200

    Update handledb.js

--- src/handledb.js
+++ src/handledb.js
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
 
 async function getAllMovies(){
-    const rawdata =  await fs.readFile('./src/moviedb.json');
+    const rawdata =  await fs.readFile('./moviedb.json');
     return JSON.parse(rawdata);
 }
 
@@ -9,7 +9,7 @@ async function addMovie(newMovie){
     const movies = await getAllMovies();
     movies.push(newMovie);
 
-    await fs.writeFile('./src/moviedb.json', JSON.stringify(movies, null, 2));
+    await fs.writeFile('./moviedb.json', JSON.stringify(movies, null, 2));
 }
 
 export {getAllMovies, addMovie};
